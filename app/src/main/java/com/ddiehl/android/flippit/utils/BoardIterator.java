b5ddd02944028e4c81f53cfe9b87895dacac1bb3
damien5314 <damien5314@gmail.com>
2014-10-23 21:48:57 -0400

    Implemented Iterator for Board objects

---
+++ app/src/main/java/com/ddiehl/android/flippit/utils/BoardIterator.java
@@ -0,0 +1,43 @@
+package com.ddiehl.android.flippit.utils;
+
+import com.ddiehl.android.flippit.game.Board;
+import com.ddiehl.android.flippit.game.BoardSpace;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class BoardIterator implements Iterator<BoardSpace> {
+	Board b;
+	int x, y;
+
+	public BoardIterator(Board b) {
+		this.b = b;
+		x = 0; y = 0;
+	}
+
+	@Override
+	public boolean hasNext() {
+		if (x >= b.width() || y >= b.height())
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public BoardSpace next() {
+		if (!hasNext())
+			throw new NoSuchElementException();
+
+		BoardSpace s = b.getSpaceAt(x, y);
+		if (++x == b.width()) {
+			y++; x = 0;
+		}
+
+		return s;
+	}
+
+	@Override
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
+}
