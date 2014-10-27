730b9a4b157a21749a1dec73cb0acb9d3c48868c
damien5314 <damien5314@gmail.com>
2014-10-26 22:41:54 -0400

    Added option to disable p2 CPU AI

--- app/src/main/java/com/ddiehl/android/flippit/activities/ReversiActivity.java
+++ app/src/main/java/com/ddiehl/android/flippit/activities/ReversiActivity.java
@@ -53,7 +53,7 @@ public class ReversiActivity extends Activity {
 		p1 = new Player(ReversiColor.White, getString(R.string.player1_label_default));
 		p1.isCPU(getResources().getBoolean(R.bool.p1_cpu));
 		p2 = new Player(ReversiColor.Black, getString(R.string.player2_label));
-		p2.isCPU(true);
+        p2.isCPU(getResources().getBoolean(R.bool.p2_cpu));
 
 		b = Board.getInstance(this);
 
