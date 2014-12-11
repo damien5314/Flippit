a17c93988e253613a3b42676ddbee211abee2dc0
damien5314 <damien5314@gmail.com>
2014-12-11 18:13:08 -0500

    Added debug text to launcher

--- app/src/main/java/com/ddiehl/android/reversi/activities/LauncherActivity.java
+++ app/src/main/java/com/ddiehl/android/reversi/activities/LauncherActivity.java
@@ -3,6 +3,7 @@ package com.ddiehl.android.reversi.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -10,6 +11,7 @@ import android.view.WindowManager;
 import com.ddiehl.android.reversi.R;
 
 public class LauncherActivity extends Activity {
+	public static final String TAG = LauncherActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,22 @@ public class LauncherActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_launcher);
+
+		Log.d(TAG, "Intent Action: " + getIntent().getAction());
+		for (String category : getIntent().getCategories())
+			Log.d(TAG, "Intent Category: " + category);
+		Log.d(TAG, "Data String: " + getIntent().getDataString());
+		Log.d(TAG, "Flags: " + getIntent().getFlags());
+
+		if (getIntent().getExtras() != null) {
+			for (String key : getIntent().getExtras().keySet())
+				Log.d(TAG, "INTENT EXTRA: " + key);
+		}
+
+		if (savedInstanceState != null) {
+			for (String key : savedInstanceState.keySet())
+				Log.d(TAG, "SAVEDINSTANCE EXTRA: " + key);
+		}
     }
 
     public void startSinglePlayer(View view) {
