72027e8fb954ea1a61db7b3b318b4b707e37adaa
Damien Diehl <damien5314@gmail.com>
2015-02-09 23:06:20 -0500

    Refactored HowToPlayActivity

--- app/src/main/java/com/ddiehl/android/reversi/view/HowToPlayActivity.java
+++ app/src/main/java/com/ddiehl/android/reversi/view/HowToPlayActivity.java
@@ -2,8 +2,10 @@ package com.ddiehl.android.reversi.view;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,19 +13,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.ddiehl.android.reversi.R;
-import com.ddiehl.android.reversi.adapters.HowToPlayPageAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created from tutorial @ http://architects.dzone.com/articles/android-tutorial-using
  */
-public class HowToPlayActivity extends FragmentActivity {
+public class HowToPlayActivity extends ActionBarActivity {
     private static final String TAG = HowToPlayActivity.class.getSimpleName();
 
-    private ViewPager pager;
-    private MenuItem previous, next;
+    private ViewPager mViewPager;
+    private MenuItem mMenuPrevious, mMenuNext;
 
     private final int[] FRAGMENT_LAYOUT_ID = new int[] {
             R.layout.activity_howtoplay_p1,
@@ -35,60 +33,72 @@ public class HowToPlayActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mViewPager = new ViewPager(this);
+        mViewPager.setId(R.id.view_pager);
         setContentView(R.layout.activity_howtoplay);
-        List<Fragment> fragments = getFragments();
-        HowToPlayPageAdapter pageAdapter = new HowToPlayPageAdapter(getSupportFragmentManager(), fragments);
-        pager = (ViewPager) findViewById(R.id.pager);
-        pager.setAdapter(pageAdapter);
-        pager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int position) {
+                return HowToPlayFragment.newInstance(FRAGMENT_LAYOUT_ID[position]);
+            }
+
+            @Override
+            public int getCount() {
+                return FRAGMENT_LAYOUT_ID.length;
+            }
+        });
+
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
             public void onPageSelected(int i) {
-                if (i == 0)
-                    previous.setEnabled(false);
-                else
-                    previous.setEnabled(true);
-
-                if (i == FRAGMENT_LAYOUT_ID.length - 1)
-                    next.setEnabled(false);
-                else
-                    next.setEnabled(true);
+                setMenuItemState(i);
             }
         });
     }
 
-    private List<Fragment> getFragments() {
-        List<Fragment> fragmentList = new ArrayList<>();
-        for (int id : FRAGMENT_LAYOUT_ID) {
-            fragmentList.add(HowToPlayFragment.newInstance(id));
+    private void setMenuItemState(int page) {
+        if (page == 0) {
+            mMenuPrevious.setEnabled(false);
+        } else {
+            mMenuPrevious.setEnabled(true);
         }
-        return fragmentList;
+
+        if (page == mViewPager.getChildCount() - 1) {
+            mMenuNext.setEnabled(false);
+        } else {
+            mMenuNext.setEnabled(true);
+        }
     }
 
     public static class HowToPlayFragment extends Fragment {
-        private final static String LAYOUT_ID = "id";
+        private final static String ARG_LAYOUT_ID = "id";
 
         public HowToPlayFragment() { }
 
         public static Fragment newInstance(int id) {
-            HowToPlayFragment frag = new HowToPlayFragment();
             Bundle args = new Bundle();
-            args.putInt(LAYOUT_ID, id);
+            args.putInt(ARG_LAYOUT_ID, id);
+            HowToPlayFragment frag = new HowToPlayFragment();
             frag.setArguments(args);
             return frag;
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(getArguments().getInt(LAYOUT_ID), container, false);
+            int layoutId = getArguments().getInt(ARG_LAYOUT_ID);
+            return inflater.inflate(layoutId, container, false);
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.how_to_play, menu);
-        previous = menu.getItem(0);
-        previous.setEnabled(false);
-        next = menu.getItem(1);
+        mMenuPrevious = menu.getItem(0);
+        mMenuNext = menu.getItem(1);
+        setMenuItemState(0);
         return true;
     }
 
@@ -96,10 +106,10 @@ public class HowToPlayActivity extends FragmentActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_previous:
-                pager.setCurrentItem(pager.getCurrentItem()-1);
+                mViewPager.setCurrentItem(mViewPager.getCurrentItem()-1);
                 break;
             case R.id.action_next:
-                pager.setCurrentItem(pager.getCurrentItem()+1);
+                mViewPager.setCurrentItem(mViewPager.getCurrentItem()+1);
                 break;
         }
         return true;
