1ab40c56d13ce978dafa568aca6f8169ebf77025
Damien Diehl <damien5314@gmail.com>
2017-03-14 19:16:46 -0700

    Revert logging stuff

--- app/src/main/java/com/ddiehl/android/reversi/Flippit.kt
+++ app/src/main/java/com/ddiehl/android/reversi/Flippit.kt
@@ -1,8 +1,6 @@
 package com.ddiehl.android.reversi
 
 import android.app.Application
-import com.ddiehl.android.logging.LogcatLogger
-import com.ddiehl.android.logging.LogcatLoggingTree
 import timber.log.Timber
 
 class Flippit : Application() {
